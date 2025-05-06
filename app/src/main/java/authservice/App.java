c587f39db3598b517df538a9b8338d5d784a6fbc
Raghav Mittal <rmittal@Raghavs-MacBook-Air.local>
2025-05-05 20:01:31 -0700

    Updated app.java and application.properties

--- app/src/main/java/authservice/App.java
+++ app/src/main/java/authservice/App.java
@@ -3,12 +3,18 @@
  */
 package authservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = {"authService.repository"})
+@ComponentScan(basePackages = {"authService.controller", "authService.auth", "authService.service"})
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
 
     public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+        SpringApplication.run(App.class, args);
+
     }
 }
